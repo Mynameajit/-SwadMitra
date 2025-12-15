fe21c3735aa79fae7a945de40005dd69249c0e18
developer_Ajit <132321923+Mynameajit@users.noreply.github.com>
2025-12-15 20:42:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 import { initPWAInstall } from "./utils/pwaInstall";
-import ThemeModeProvider from './context/ThemeModeProvider.jsx'
+import ThemeModeProvider from './context/ThemeModeProvider .jsx'
 
 // 🔥 Init PWA install banner
 initPWAInstall();
